6df0863c5de3ff0f50ea883e4d80b351db5e0409
Ajay Mamgain <ajay.mamgain2007@gmail.com>
2025-04-10 17:35:07 +0530

    Add webpack configuration for Lambda functions

---
+++ lambda/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+
+module.exports = {
+  entry: slsw.lib.entries,
+  target: 'node',
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  optimization: {
+    minimize: false,
+  },
+  performance: {
+    hints: false,
+  },
+  devtool: 'source-map',
+  externals: [
+    /aws-sdk/, // Exclude aws-sdk as it's provided by Lambda
+    /playwright-core/, // Large dependency, will be included via layers
+  ],
+  resolve: {
+    extensions: ['.js', '.json'],
+  },
+  output: {
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+    sourceMapFilename: '[file].map',
+  },
+};
